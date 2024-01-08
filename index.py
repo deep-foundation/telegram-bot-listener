279d8d24f18024c01bdf0e1813c623f4ffd0ce8b
trooper2003 <alexander.bykoff2016@yandex.ru>
2024-01-08 23:12:53 +0600

    add /init route

--- index.py
+++ index.py
@@ -53,28 +53,6 @@ def init():
     return jsonify({})
 
 
-@app.route('/call', methods=['POST'])
-def call():
-    try:
-        body = request.json
-        params = body['params']
-        full_code = TEMPLATE_CODE.replace("{{USER_CODE}}", params['code'])
-        import_section, code_section = separate_imports(full_code)
-        exec(import_section, globals())
-        args = {
-            'deep': make_deep_client(params['jwt']),
-            'data': params['data'],
-        }
-        python_handler_context = {'args': args}
-        code_object = compile(code_section, 'python_handler', 'exec')
-        exec(code_object, globals(), dict(python_handler_context=python_handler_context))
-        result = python_handler_context['result']
-        print(f"Resolved: {result}")
-        return jsonify({'resolved': result})
-    except Exception as e:
-        print(f"Rejected: {traceback.format_exc()}")
-        return jsonify({'rejected': traceback.format_exc()})
-
 if __name__ == '__main__':
     asgi_app = WsgiToAsgi(app)
     uvicorn.run(asgi_app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)), log_level="info", reload=False)
